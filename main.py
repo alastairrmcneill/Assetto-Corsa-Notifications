f817e7f90a59aa6d81b8183d1bdf2c20560c9c05
Ali <alastair.r.mcneill@gmail.com>
2021-10-14 18:13:46 +0100

    Scraped best time and printed

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ username_field.send_keys(username)
 password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
 
+driver.get('http://149.7.16.11:8840/live-timing')
+time.sleep(1)
 
-time.sleep(3)
+table = driver.find_element_by_id("live-table-disconnected")
+
+best_row = table.find_element_by_class_name("driver-row")
+best_driver = best_row.find_element_by_class_name("driver-name")
+best_lap = best_row.find_element_by_class_name("best-lap")
+
+print(best_driver.text)
+print(best_lap.text)
+
 driver.quit()
