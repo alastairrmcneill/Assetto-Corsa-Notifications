4feec85ff576e5f74b48933ecd3fcb77396741ef
Ali <alastair.r.mcneill@gmail.com>
2021-10-23 13:47:09 +0100

    Added in json log

--- main.py
+++ main.py
@@ -3,16 +3,40 @@ from selenium.webdriver.common.keys import Keys
 from credentials import ACSERVER, GOOGLE
 import os
 import time
+import json
 import smtplib
 from datetime import datetime
 
-
+# Variables
+leader = {'Track': '',
+          'Car': '',
+          'Driver': '',
+          'Lap time': '',
+          'Lap time number': -1}
 
 # Functions
 def convert_text_to_time(time_text):
     time_obj = datetime.strptime(time_text,'%M:%S.%f')
     time_num = time_obj.minute*60 + time_obj.second + float(time_obj.microsecond/1000000)
     return time_num
+
+def send_email(subject_text):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(GOOGLE.email, GOOGLE.app_password)
+
+        body = ''
+
+        msg = f'Subject: {subject_text} \n\n {body}'
+
+        sender = GOOGLE.email
+        receiver = GOOGLE.email
+
+        smtp.sendmail(sender, receiver, msg)
+
 
 
 # Gather data
@@ -28,10 +52,24 @@ username_field.send_keys(ACSERVER.username)
 password_field.send_keys(ACSERVER.password)
 password_field.send_keys(Keys.RETURN)
 
+
+# Race info
+driver.get("http://149.7.16.11:8840/")
+
+try:
+    event = driver.find_element_by_class_name("race-info")
+    track = event.find_element_by_xpath("//div[@class='race-info']//strong")
+    cars = event.find_elements_by_xpath("//div[@class='race-info']//small")
+    car = cars[-1]
+    leader['Track'] = track.text
+    leader['Car'] = car.text
+except:
+    print("No event")
+
+
+# Get timings
 driver.get('http://149.7.16.11:8840/live-timing')
 time.sleep(1)
-
-
 
 # Live best time
 try:
@@ -61,11 +99,15 @@ except:
 
 if live_best_time_num > 0 or offline_best_time_num >0:
     if live_best_time_num > offline_best_time_num:
-        leader = {"Driver": live_best_driver,
-                  "Lap time": live_best_time}
+        leader['Driver'] = live_best_driver
+        leader['Lap time'] = live_best_time
+        leader['Lap time number'] = live_best_time_num
+
     elif offline_best_time_num > live_best_time_num:
-        leader = {"Driver": offline_best_driver,
-                  "Lap time": offline_best_time}
+        leader['Driver'] = offline_best_driver
+        leader['Lap time'] = offline_best_time
+        leader['Lap time number'] = offline_best_time_num
+
     else:
         print("There is a tie")
         leader = {}
@@ -78,27 +120,40 @@ driver.quit()
 
 
 
-# Send notification
+# Compare to previous best time
+with open('results.json', 'r') as f:
+    data = json.load(f)
 
+event = data["Events"][-1]
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+if event["Track"] == leader["Track"] and event["Car"] == leader["Car"]:
+    if leader["Lap time number"] < event["Lap time number"]:
+        print("This is a new lap record")
 
-    smtp.login(GOOGLE.email, GOOGLE.app_password)
+        # Send email
+        subject = "The current leader is " + leader["Driver"] + " with a time of " + leader["Lap time"] + "."
+        send_email(subject)
 
+        # Update event
+        event = leader
+
+        # Update data
+        data["Events"][-1] = event
+
+        # Save to json file
+        with open('results.json', 'w') as f:
+            json.dump(data, f)
+
+else: # It is a new event
+    # Send email
     subject = "The current leader is " + leader["Driver"] + " with a time of " + leader["Lap time"] + "."
-    body = ''
+    send_email(subject)
 
-    msg = f'Subject: {subject} \n\n {body}'
+    data["Events"].append(leader)
 
-    sender = GOOGLE.email
-    receiver = GOOGLE.email
-
-    smtp.sendmail(sender, receiver, msg)
-
-print(subject)
+    # Save to json file
+    with open('results.json', 'w') as f:
+        json.dump(data, f)
 
 
 
